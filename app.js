2d8f4c8d61fd70f10bd3e0efc01debfd94b43b05
jahallb1 <62314714+jahallb1@users.noreply.github.com>
2021-01-27 15:36:02 -0500

    finished css and adding js to game

---
+++ app.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const userGrid = document.querySelector(".grid-user");
+  const computerGrid = document.querySelector(".grid-computer");
+  const displayGrid = document.querySelector(".grid-display");
+  const ship = document.querySelectorAll(".ship");
+  const destroyer = document.querySelector(".destroyer-container");
+  const submarine = document.querySelector(".submarine-container");
+  const cruiser = document.querySelector(".cruiser-container");
+  const battleship = document.querySelector(".battleship-container");
+  const carrier = document.querySelector(".carrier-container");
+  const StartButton = document.querySelector("#start");
+  const rotateButton = document.querySelector("#rot ");
+});
